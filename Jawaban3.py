2cdd62f164d21edb9a3acd60466526c7a40c5387
BagusFadhilah <117616453+BagusFadhilah@users.noreply.github.com>
2022-11-07 14:56:49 +0700

    UTS

---
+++ Jawaban3.py
@@ -0,0 +1,13 @@
+print("============")
+print("program Quiz")
+print("============")
+
+Nama = str(input ("Silahkan Masukan Nama Anda : "))
+NIM  = str(input ("Silahkan Masukan NIM Anda : "))
+
+print("")
+print("================")
+print("\nData Mahasiswa")
+print("================")
+print("Nama :" + Nama)
+print("NIM  :" + NIM)
