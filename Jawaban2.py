d97611ebfa0241c09f62ee9954a9a39eb53f3b2e
BagusFadhilah <117616453+BagusFadhilah@users.noreply.github.com>
2022-11-07 14:56:16 +0700

    UTS

---
+++ Jawaban2.py
@@ -0,0 +1,10 @@
+print("")
+print("Program Python Menghitung Luas Segitiga")
+print("=======================================")
+
+a = int(input("\nMasukan Nilai Alas   : "))
+t = int(input("\nMasukan Nilai Tinggi : "))
+
+luas = 0.5 * a * t
+
+print("\nHasil Luas Segitiga  = %0.2f"% luas)
